d237aaeed921e7ee15615b8e61994fcfcff14c11
Dmitrii <dmitriialekseenkoog@gmail.com>
2022-11-21 15:20:22 +0400

    15 lesson

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import qs from 'qs';
 
 //Components
 import Categories from '../components/Categories';
@@ -9,12 +11,17 @@ import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import { Pagination } from '../components/Pagination';
 import { SearchContext } from '../App';
+import { list } from '../components/Sort';
 
 //Redux
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 
 function Home() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isSearch = useRef(false);
+  const isMounted = useRef(false);
+
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
 
   const { searchValue } = useContext(SearchContext);
@@ -30,32 +37,68 @@ function Home() {
     dispatch(setCurrentPage(number));
   };
 
-  useEffect(() => {
-    async function fetchPizzas() {
-      try {
-        setIsLoading(true);
+  const fetchPizzas = async () => {
+    try {
+      setIsLoading(true);
 
-        const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
-        const sortBy = sort.sortProperty.replace('-', '');
-        const category = categoryId > 0 ? `&category=${categoryId}` : ``;
-        const search = searchValue ? `&search=${searchValue}` : '';
+      const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+      const sortBy = sort.sortProperty.replace('-', '');
+      const category = categoryId > 0 ? `&category=${categoryId}` : ``;
+      const search = searchValue ? `&search=${searchValue}` : '';
 
-        await axios
-          .get(
-            `https://634936fca59874146b1a394b.mockapi.io/pizzas?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`,
-          )
-          .then(({ data }) => {
-            console.log(data);
-            setPizzas(data);
-            setIsLoading(false);
-          });
-      } catch (error) {
-        alert('Произошла ошибка при загрузке данных');
-        console.log(error);
-      }
+      await axios
+        .get(
+          `https://634936fca59874146b1a394b.mockapi.io/pizzas?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`,
+        )
+        .then(({ data }) => {
+          console.log(data);
+          setPizzas(data);
+          setIsLoading(false);
+        });
+    } catch (error) {
+      alert('Произошла ошибка при загрузке данных');
+      console.log(error);
     }
-    fetchPizzas();
+  };
+
+  useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      });
+
+      navigate(`?${queryString}`);
+    }
+    isMounted.current = true;
+  }, [categoryId, sort.sortProperty, searchValue, currentPage, navigate]);
+
+  //Если был первый рендер, то проверяем URL параметры и сохраняем в Redux
+  useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+
+      const sort = list.find((obj) => obj.sortProperty === params.sortProperty);
+
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        }),
+      );
+      isSearch.current = true;
+    }
+  }, []);
+
+  useEffect(() => {
     window.scrollTo(0, 0);
+
+    if (!isSearch.current) {
+      fetchPizzas();
+    }
+
+    isSearch.current = false;
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
   const items = pizzas.map((item) => <PizzaBlock key={item.id} {...item} />);
